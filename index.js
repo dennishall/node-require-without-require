439ca6399c8b45af75a8d19b759f1012d7929800
Dennis Hall <hall.dennis+github@gmail.com>
2020-10-06 07:44:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 // const Sum = require('./includes/Sum'); // if Sum.js had `module.exports = Sum`, this would work.
 
 // "require without require"
-if (fs.existsSync('./includes/Sum.js')) {
-    eval(fs.readFileSync('./includes/Sum.js', 'utf8'));
-}
+eval(fs.readFileSync('./includes/Sum.js', 'utf8'));
 
 // now we have the function available from the required file.
 console.log( Sum(2, 2) );
